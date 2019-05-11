1a0ace179315327d60155f92551ec4b67d2920ed
iqux_anlve@163.com <lllxu251314>
2019-05-11 09:30:46 +0800

    modify somethings

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Anlve.", "Developer.", "Amateur photographer.", "Freelancer.", "Game lovers"],
       typeSpeed: 100,
       loop: true,
     });
